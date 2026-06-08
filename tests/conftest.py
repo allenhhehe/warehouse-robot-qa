672d75a92d9973de3ebd460282a3020b04c3d351
aboutallen <aboutallen19850101@gmail.com>
2026-06-08 20:37:04 +0800

    fix import path and project structure

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,8 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 import pytest
-from robot_scheduler import RobotScheduler
+from app.robot_scheduler import RobotScheduler
 
 @pytest.fixture
 def scheduler():
